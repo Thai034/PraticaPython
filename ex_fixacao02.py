fa6a15c51c422737f967b688648b349abdd68ccf
Thaila Alves <thailasalvees@gmail.com>
2023-08-01 11:20:39 -0300

    Resolução do exercício 2

---
+++ ex_fixacao02.py
@@ -0,0 +1,5 @@
+print("Coloque o valor da sua conta do restaurante, para calcularmos o total com o acréscimo ao garçom 😊")
+num = float(input("Valor da sua conta: "))
+
+res = num + (num * 0.10)
+print("Valor total a pagar: ", res)
